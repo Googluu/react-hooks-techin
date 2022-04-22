c28b4ff23948c8d7511cdf3e6751854915249d90
GlooGluu <sneyder.h.rodriguez@gmail.com>
2022-04-22 13:53:45 -0500

    [Hooks] three pilares ⚖

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+import Characters from './components/Characters';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello World</h1>
+     <Header />
+     <Characters />
+    </div>
+  );
+}
+
+export default App;
